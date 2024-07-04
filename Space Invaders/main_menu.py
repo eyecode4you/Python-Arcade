39c50a7d49d312d507f7f0069e4e87494a66139c
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 09:47:10 +0100

    Update main_menu.py

--- Space Invaders/main_menu.py
+++ Space Invaders/main_menu.py
@@ -5,13 +5,8 @@ class MainMenu:
         self.font = pygame.font.Font('fonts/Cyber-BoldRustique.ttf', 80)
         self.title = self.font.render("Py Invaders", True, (255, 255, 255))
         self.title_position = (10, 10)
-        self.second_menu = None
     
     def update(self, events):
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    return self.second_menu
         return self
 
     def draw(self, screen):
