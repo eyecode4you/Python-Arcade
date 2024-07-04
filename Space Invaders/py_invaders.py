2a46d6d6032001098c06bd1655ac7c5ae9db64cb
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-04 09:43:50 +0100

    Update py_invaders.py

--- Space Invaders/py_invaders.py
+++ Space Invaders/py_invaders.py
@@ -1,12 +1,7 @@
 from game import Game
 from main_menu import MainMenu
-from second_menu import SecondMenu
 
 game = Game("PyInvaders", 800, 800)
 main_menu = MainMenu()
-second_menu = SecondMenu()
-
-main_menu.second_menu = second_menu
-second_menu.main_menu = main_menu
 
 game.run(main_menu)
